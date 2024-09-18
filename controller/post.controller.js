734e8753d61cb1e7ac181af5e582d46c2b6916e3
Nikita Borisov <qfana2123@gmail.com>
2024-09-19 00:51:19 +0300

    add post with user

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,1 +1,19 @@
-// Coming soon
+const db = require('../db');
+
+class PostController {
+  async createPost(req, res) {
+    const { title, content, user_id } = req.body;
+    const newPost = await db.query('INSERT INTO post (title, content, user_id) values ($1, $2, $3) RETURNING *', [title, content, user_id]);
+
+    res.json(newPost.rows);
+  }
+
+  async getPostByUser(req, res) {
+    const id = req.query.id;
+    const posts = await db.query('SELECT * FROM post where user_id = $1', [id]);
+
+    res.json(posts.rows);
+  }
+}
+
+module.exports = new PostController();
